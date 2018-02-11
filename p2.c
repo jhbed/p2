1a62f51416a0570448dc0edce648424ae69546bf
jhbed <jakehbedard@gmail.com>
2018-02-11 14:08:50 -0800

    managed to get a persistent shell that can run echo and ls

--- p2.c
+++ p2.c
@@ -1,9 +1,7 @@
 /*Includes two functions: parse() and main()*/
 
 #include "p2.h"
-#include "getword.h"
-#include <stdlib.h>
-#include <unistd.h>
+
 
 /*GLOBAL*/
 /*an array of pointers (addresses for chars)*/
@@ -11,21 +9,55 @@ char *newargv[MAXITEM];
 char w[STORAGE*MAXITEM];
 
 
+// FUNCTIONAL MAIN - IF ALL ELSE FAILS REVERT BACK TO THIS
+
+// int main(){
+	
+// 	int words;
+
+// 	for(;;) {
+// 		//this will exit the program because you don't start a child process!!!
+// 		words = parse();
+// 		if (words == 0) break;
+// 		execvp(newargv[0], newargv);
+// 	}
+// 	return 0;
+// }
+
 
 
 int main(){
 	
 	int words;
+	int kidpid; //variable used to designate the child
 
 	for(;;) {
 		//this will exit the program because you don't start a child process!!!
+		printf("p2: ");
 		words = parse();
-		if (words == 0) break;
-		execvp(newargv[0], newargv);
+		
+		if (words == -10) break;
+		// execvp(newargv[0], newargv);
+		if(-1 == (kidpid = (int) fork())){ //if fork returns -1 it failed
+			perror("Fork unsuccessful");
+			exit(EXIT_FAILURE);
+		} else if (0 == kidpid) { // if fork returns 0 that means we are the child
+			
+			// INSERT THINGS WE WANT THE CHILD TO DO
+			execvp(newargv[0], newargv);
+			perror("execve");   /* execve() returns only on error */
+            exit(EXIT_FAILURE);
+
+		} else { // WE ARE THE PARENT, we return the PID of the child we created...
+
+			wait(NULL);
+			
+			//exit(EXIT_SUCCESS);
+		}
 	}
+	kill(getpid(), SIGTERM);
 	return 0;
 }
-
 
 
 /*
@@ -53,8 +85,8 @@ int parse(){
 	//memory slot = slot w
 
 	/*pass memory location slots to getword */
-	while((c = getword(w + moveForward)) != -10){
-		if (c == 0) break;
+	//if getword returns &, EOF, or newline we want to stop getting words and execute
+	while((c = getword(w + moveForward)) > 0){
 
 		newargv[index] = w + moveForward; //set newargv[index] = address of start of word
 		index++; 
--- p2.h
+++ p2.h
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #include "getword.h"
+
 #define MAXITEM 100 /*max number of words per line*/
 int parse();
